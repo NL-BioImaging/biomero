e053babf6b4c3290847265f7c6098013579dbce3
Torec Luik <t.t.luik@amc.uva.nl>
2023-06-07 13:47:31 +0200

    Add dependencies for old Python

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@ from setuptools import setup
 
 setup(name="omero_slurm_client",
       use_scm_version=True,
-      setup_requires=['setuptools_scm'])
+      setup_requires=['setuptools_scm'],
+      install_requires=[
+            # "requests==2.31.0", # needs Python3.7+, which will use the toml instead
+            "requests==2.27.1",
+            "fabric==3.1.0",
+            "paramiko==3.2.0",
+            "importlib_resources>=5.4.0"
+      ])

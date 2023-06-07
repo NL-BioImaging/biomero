dea4ffaed408b53b342d821dcc9426eea61d837d
Torec Luik <t.t.luik@amc.uva.nl>
2023-06-07 12:09:33 +0200

    Add req

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 import os
 import sys
 from importlib.metadata import version as dynamicversion
-import omero_slurm_client
+# import omero_slurm_client
 sys.path.append(os.path.abspath('../..'))
 sys.path.append(os.path.abspath('../omero_slurm_client'))
 sys.path.append(os.path.abspath('..'))

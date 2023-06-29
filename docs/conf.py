974dcd155f84be3f9beea3b19f2781cd8e50866c
Torec Luik <t.t.luik@amc.uva.nl>
2023-06-29 16:08:49 +0200

    Up docs

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ html_theme_options = {
     # 'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
-    # 'sticky_navigation': True,
+    'sticky_navigation': True,
     # 'navigation_depth': 4,
     # 'includehidden': True,
     # 'titles_only': False

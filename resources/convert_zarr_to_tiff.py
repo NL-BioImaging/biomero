7cc6f2ba8bdff7b64d9437b390f92f01f1791492
Torec Luik <t.t.luik@amc.uva.nl>
2023-10-04 14:03:58 +0200

    Reverse dimension order from zarr

--- resources/convert_zarr_to_tiff.py
+++ resources/convert_zarr_to_tiff.py
@@ -29,13 +29,21 @@ def convert_zarr_to_tiff(zarr_file_path, key=None, output_file=None):
                 raise ValueError(f"Specified key '{key}' not found in the Zarr file.")
 
             # Create a Dask array from the specified Zarr key (Ready for persist)
-            dask_image_data = da.from_zarr(zarr_file[key]).persist()
+            dask_image_data = da.from_zarr(zarr_file[key])
+            
+        # ZARR extracted from OMERO will be (up to) 5D arrays of shape (t, c, z, y, x). 
+        # But with 2D images that is (t, c, y, x), or (c, y, x)
+        # We want to provide the opposite, (x, y, c, (t)). So invert all dimensions:
+        num_dims = len(dask_image_data.shape)
+        new_order = tuple(range(num_dims - 1, -1, -1))  # Reverses the dimensions
+        dask_image_data = dask_image_data.transpose(new_order)
 
         # Generate the default output file name based on the input file name and key
         if output_file is None:
             output_file = os.path.splitext(zarr_file_path)[0] + f".{key}.tif"
 
         # Write the Dask array to the TIFF file
+        dask_image_data.persist()
         tf.imwrite(output_file, dask_image_data)
         
         logger.info(f"Conversion completed successfully with key: '{key}'.")

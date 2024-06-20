812e3fdd948ec996d75c29f4d5fa903514645fe1
Luik <t.t.luik@amsterdamumc.nl>
2024-06-20 12:39:05 +0200

    For Python3.7 use pkg_resources instead

--- biomero/__init__.py
+++ biomero/__init__.py
@@ -1,8 +1,14 @@
 from .slurm_client import SlurmClient
-import importlib.metadata
 
-# Get the version from pyproject.toml
 try:
-    __version__ = importlib.metadata.version(__package__)
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "Version not found"
+    import importlib.metadata
+    try:
+        __version__ = importlib.metadata.version(__package__)
+    except importlib.metadata.PackageNotFoundError:
+        __version__ = "Version not found"
+except ModuleNotFoundError:  # Python 3.7
+    try:
+        import pkg_resources
+        __version__ = pkg_resources.get_distribution(__package__).version
+    except pkg_resources.DistributionNotFound:
+        __version__ = "Version not found"

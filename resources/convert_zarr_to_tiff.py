cb406f1805c1f248dffe12e32f4557138f265fac
Maarten Paul <maarten.paul@gmail.com>
2025-03-13 14:38:46 +0100

    add licence

--- resources/convert_zarr_to_tiff.py
+++ resources/convert_zarr_to_tiff.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Torec Luik, Maarten Paul
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import os
 import zarr
@@ -165,7 +179,7 @@ def convert_zarr_to_tiff(zarr_file_path, key=None, output_file=None):
             logger.info(f"Original data shape: {dask_image_data.shape}")
 
             if output_file is None:
-                #to make it easier to work with current BIOMERO workflow save as tif instead of ome.tif 
+                #to make it easier to work with current BIOMERO workflow save as .tif instead of ome.tif 
                 output_file = os.path.splitext(zarr_file_path)[0] + f".{key}.tif"
 
             # Create metadata with actual dimension order

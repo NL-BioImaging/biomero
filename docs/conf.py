5a6f851c083d12d945a8b204d9f68bd3c3fb72ba
Luik <t.t.luik@amsterdamumc.nl>
2026-09-23 10:36:49 +0200

    docs: Add GitHub source links

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,15 @@ intersphinx_mapping = {
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+# Native sphinx-rtd-theme "Edit on GitHub" link.
+html_context = {
+    'display_github': True,
+    'github_user': 'NL-BioImaging',
+    'github_repo': 'biomero',
+    'github_version': 'main',
+    'conf_py_path': '/docs/',
+}
+
 # Search configuration - ensure search index is built
 html_search_language = 'en'
 
